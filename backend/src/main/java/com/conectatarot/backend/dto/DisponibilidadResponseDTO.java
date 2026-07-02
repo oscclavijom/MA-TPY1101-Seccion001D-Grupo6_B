46b04978902aeb7be411d9a2b5fd322f8a19df18
oscclavijom <oscclavijom@gmail.com>
2026-07-02 03:26:58 -0400

    arreglando disponibilidad agendar sesion

--- backend/src/main/java/com/conectatarot/backend/dto/DisponibilidadResponseDTO.java
+++ backend/src/main/java/com/conectatarot/backend/dto/DisponibilidadResponseDTO.java
@@ -1,10 +1,12 @@
 package com.conectatarot.backend.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class DisponibilidadResponseDTO {
 
     private String diaSemana;
